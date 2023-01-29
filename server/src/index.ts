13da069b7b5c7fe4a127bfc2488c72a97a08eee2
Shahrad <shahrad_m@icloud.com>
2023-01-29 00:10:44 -0500

    create router for the API calls async

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,9 @@ import "reflect-metadata";
 import { createServer } from 'http';
 import { config } from 'dotenv';
 import { source } from './data-source';
-
+import { request } from 'http';
 import helloController from './modules/hello/hello.controller';
-
-const cohere = require('cohere-ai');
-
-cohere.init('3DNJDqC2p7HaKjQduEb7RchGymNMgxWP3giiJhLF'); // This is your trial API key
+import uploadController from './modules/upload/upload.controller';
 
 config();
 source.initialize();
@@ -25,93 +22,7 @@ app.use(bodyParser.json());
 const http = createServer(app);
 
 app.use('/hello', helloController);
-
-const item = 'Milk';
-
-
-// the API call for the cohere.ai model
-(async () => {
-  const response = await cohere.generate({
-    model: 'command-xlarge-20221108',
-    prompt: 'In what broad category of groceries do you categorize' + item + 'if you saw it on a receipt? In one word please.',
-    max_tokens: 300,
-    temperature: 0.9,
-    k: 0,
-    p: 0.75,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop_sequences: [],
-    return_likelihoods: 'NONE'
-  });
-  console.log(`Prediction: ${response.body.generations[0].text}`);
-})();
-
-
-// import requests
-// import base64
-
-const CLIENT_ID = "CLIENT_ID";
-
-const ENVIRONMENT_URL = "ENVIRONMENT_URL";
-
-const username = "shahrad2002";
-const api_key = "2360a1fd67fa93d3cb613093d26868b5";
-
-const process_image_url =  ENVIRONMENT_URL  + 'api/v8/partner/documents/';
-
-const headers = {
-    "Content-Type": "application/json", 
-    "Accept": "application/json",
-    "CLIENT-ID": CLIENT_ID,
-    "AUTHORIZATION": "apikey " + username + ": " + api_key
-}
-
-import * as fs from 'fs';
-// TODO: download and install the packages
-import * as base64 from 'base64-arraybuffer';
-import * as request from 'request-promise-native';
-
-// file path and file name
-const imagePath = '/tmp/example.jpg';
-const fileName = 'example.jpg';
-
-// read image file and convert to ArrayBuffer
-const imageFile = fs.readFileSync(imagePath);
-const imageBuffer = new Uint8Array(imageFile).buffer;
-
-// convert image to Base64
-const base64EncodedString = base64.encode(imageBuffer);
-
-// define categories
-const categories = ["Office Expense", "Meals & Entertainment", "Utilities", "Auto"];
-
-// create payload
-const payload = {
-    file_name: fileName,
-    file_data: base64EncodedString,
-    categories: categories
-};
-
-// send payload in a POST request
-const options = {
-    method: 'POST',
-    uri: process_image_url,
-    headers: headers,
-    json: payload
-};
-
-request(options)
-    .then((response: any) => {
-        console.log(response);
-    })
-    .catch((err: any) => {
-        console.error(err);
-    });
-
-
-
-
-
+app.use('/upload', uploadController);
 
 http.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ server/src/modules/upload/upload.controller.ts
@@ -0,0 +1,54 @@
+import { Router } from 'express';
+import { source } from '../../data-source';
+import { Receipt } from '../../entity/Receipt';
+import * as fs from 'fs';
+const cohere = require('cohere-ai');
+const router = Router();
+const Client = require('@veryfi/veryfi-sdk');
+
+
+router.get('', async (req, res) => {
+
+    // veryFi API credentials 
+    const verifyClientID = "vrftuS92oT4eloR42BbvpmRTyHEIUUAMMxWJTSN";
+    const verifyClientSECRET = "spl5t64dIVUEn4TVmM0it1wL03d74CPLNIuGn3BRWVBZmiSHfSjiO5iaMPfDfY2ks9TzU4EEhDNxpFmVSl9vRRGWDYXabOZjGkbAv5u5y1rkXjhIdxz77abz1D3rjN2j";
+    const verifyUsername = 'shahrad2002';
+    const apiKey = '2360a1fd67fa93d3cb613093d26868b5';
+    cohere.init('3DNJDqC2p7HaKjQduEb7RchGymNMgxWP3giiJhLF');
+    const categories = ['Grocery', 'Utilities', 'Travel'];
+
+    const base64img = req.body.receipt; // base64 image of the receipt
+
+    let veryfi_client = new Client(verifyClientID, verifyClientSECRET, verifyUsername, apiKey);
+    let verifyResponse = await veryfi_client.process_document_buffer(base64img); // receipt OCR
+    console.log(verifyResponse);    // print the response
+
+    const receiptRepository = source.getRepository(Receipt);    // get the receipt repository
+    const receipt = receiptRepository.create({ date: Date.parse(verifyResponse.date), tax: verifyResponse.tax, vendor: verifyResponse.vendor.stringify() }); // create a new receipt
+
+    // save each receipt item on the database
+    for (let i = 0; i < verifyResponse.line_items.length; i++) {
+        const item = verifyResponse.line_items[i].description;
+        const cohereResponse = await cohere.generate({
+            model: 'command-xlarge-20221108',
+            prompt: 'In what broad category of groceries do you categorize' + item + 'if you saw it on a receipt? In one word please.',
+            max_tokens: 300,
+            temperature: 0.9,
+            k: 0,
+            p: 0.75,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop_sequences: [],
+            return_likelihoods: 'NONE'
+        });
+        // save the stuff on the entities table for each item
+
+        const tag = `${cohereResponse.body.generations[0].text}`;
+        console.log(item);
+    }
+    res.json(receipt);
+});
+
+export default router;
+
+
